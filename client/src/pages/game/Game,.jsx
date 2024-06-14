08570a98465ad78484b8ea322f01afa85c4ce895
piotrkowalczykk <kowalczyk.piotrt@gmail.com>
2024-06-14 11:48:39 +0200

    Added layout of game page

--- client/src/pages/game/Game,.jsx
+++ client/src/pages/game/Game,.jsx
@@ -1,15 +1,25 @@
 import styles from "./Game.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowsRotate, faHourglassStart, faThumbsUp, faThumbsDown} from "@fortawesome/free-solid-svg-icons";
 function Game(){
 
     const players = JSON.parse(localStorage.getItem("players") || "[]");
-    console.log(players)
 
     return(
         <>
         <div className={styles.container}>
             <div className={styles.question}>
-                <label className={styles.questionFor}></label>
-                <p className={styles.text}></p>
+                <div className={styles.questionFor}>Siema</div>
+                <div className={styles.text}></div>
+                <div className={styles.timerContainer}>
+                    <div className={styles.timer}></div>
+                </div>
+                <div className={styles.buttons}>
+                    <button className={styles.flipBtn}><FontAwesomeIcon className={styles.fix} icon={faArrowsRotate} /> SHOW</button>
+                    <button className={styles.startTimerBtn}><FontAwesomeIcon className={styles.fix} icon={faHourglassStart} /> START</button>
+                    <button className={styles.yesBtn}><FontAwesomeIcon className={styles.fix} icon={faThumbsUp} /></button>
+                    <button className={styles.noBtn}><FontAwesomeIcon className={styles.fix} icon={faThumbsDown} /></button>
+                </div>
             </div>
         </div>
         <div className={styles.playersScore}>
